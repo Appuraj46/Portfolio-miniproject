db334f389279fa09518ac7771773ead3c53c5f62
Appuraj gs <user.appurajgs2005@gmail.com>
2024-12-22 16:03:40 +0530

    pushing project

---
+++ script.js
@@ -0,0 +1,6 @@
+function phMenu(){
+    const menu = document.querySelector('.phmenu-links');
+    const icon = document.querySelector('.phmenu-icon');
+    menu.classList.toggle('open');
+    icon.classList.toggle('open');
+}
